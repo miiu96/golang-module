9946d1da0efce6ec0ac0ea0940fe6ef768350cff
miiu96 <mihai9625@gmail.com>
2021-06-16 10:45:04 +0300

    first commit

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/miiu96/golangmodule_v0
+
+go 1.15
+
+require github.com/ElrondNetwork/elrond-go-logger v1.0.4
---
+++ sum.go
@@ -0,0 +1,9 @@
+package golang_module
+
+import logger "github.com/ElrondNetwork/elrond-go-logger"
+
+func WriteSomethingV1(str string) {
+	log := logger.GetOrCreate("core/indexer")
+
+	log.Warn("Something", "str", str)
+}
